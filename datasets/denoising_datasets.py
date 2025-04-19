69bc33f608755dea34cabe3f096c6fd70da69d25
keinsam <kein8sam@gmail.com>
2025-04-19 18:39:22 +0200

    denoising dataset implementation

---
+++ datasets/denoising_datasets.py
@@ -0,0 +1,14 @@
+import torch
+from torch.utils.data import Dataset
+from base_datasets.py import BaseCIFAR10
+
+class DenoisingCIFAR10(BaseCIFAR10) :
+    def __init__.self(root, train, noise, transform=None) :
+        super().__init__(root, train, transform)
+        assert noise > 0
+        self.noise = noise
+
+    def __getitem__(self, idx) :
+        image, label = super().__getitem__(idx)
+        noisy_image = torch.poisson(image * self.noise) / self.noise
+        return noisy_image, image
